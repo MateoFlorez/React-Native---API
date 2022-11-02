5dce48efaae8b6ac4f18ecb07b2df4e68d1ec731
203 <203@academicos.cesde.edu.co>
2022-11-02 08:51:54 -0500

    Consumo api con axios

--- App.js
+++ App.js
@@ -1,63 +1,154 @@
 import { StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [idsearch, setIdsearch] = useState('');
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
+  // const [name, setName] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [apellidos, setApellidos] = useState('');
+  // const [username, setUsername] = useState('');
 
-  const getUsers = async () => {
+  // const getUsers = async () => {
+  //   try {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  //     const json = await response.json();
+  //     setData(json);
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
+  // const getUserById = async (id) => {
+  //   try {
+  //     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  //     const json = await response.json();
+  //     setData(json);
+  //     // Chequear si se encuentra el id
+
+  //     if (json.name != null) {
+  //       setName(json.name); // Actualizar el estado name
+  //       setUsername(json.username) // Actualizar el estado username
+  //     }
+  //     else {
+  //       alert('Id NO encontrado')
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
+  const getClientes = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users');
-      const json = await response.json();
-      setData(json);
+      const url = `http://172.16.59.144:3000/api/clientes`;
+      const response = await axios.get(url);
+      setData(response.data)
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
+  };
+
+  const getClientById = async (id) => {
+    try {
+      const url = `http://172.16.59.144:3000/api/clientes/${id}`;
+      const response = await axios.get(url);
+      //setData(response.data)
+      setNombre(response.data.nombre);
+      setApellidos(response.data.apellidos);
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
+  };
+
+  const saveCliente = async () => {
+    if (!nombre.trim() || !apellidos.trim()) {
+      alert("Nombre y apellidos inválidos");
+      return;
+    }
+    setLoading(true);
+    try {
+      const response = await axios.post(`http://172.16.59.144:3000/api/clientes`, {
+        nombre,
+        apellidos,
+      });
+      alert("Cliente agregado correctamente ...")
     } catch (error) {
-      console.error(error);
-    } finally {
+      console.log(error)
+    }
+    finally {
       setLoading(false);
     }
-  }
+  };
 
-  const getUserById = async (id) => {
+  const updateCliente = async (id) => {
+    if (!nombre.trim() || !apellidos.trim()) {
+      alert("Nombre y apellidos inválidos");
+      return;
+    }
+    setLoading(true);
     try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-      const json = await response.json();
-      setData(json);
-      // Chequear si se encuentra el id
-
-      if (json.name != null) {
-        setName(json.name); // Actualizar el estado name
-        setUsername(json.username) // Actualizar el estado username
-      }
-      else {
-        alert('Id NO encontrado')
-      }
+      const response = await axios.put(`http://172.16.59.144:3000/api/clientes/${id}`, {
+        nombre,
+        apellidos,
+      });
+      alert("Cliente actualizado correctamente ...")
     } catch (error) {
-      console.error(error);
-    } finally {
+      console.log(error)
+    }
+    finally {
       setLoading(false);
     }
-  }
+  };
+
+  const deleteCliente = async (id) => {
+    try {
+      if (confirm("Está seguro de eliminar este cliente")){
+        const response = await axios.delete(`http://172.16.59.144:3000/api/clientes/${id}`, {
+        });
+        alert("Cliente Eliminado exitosamente ...")
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setLoading(false);
+    }
+  };
+
 
   useEffect(() => {
     // getUsers(); // Al cargar el componente por primera vez 
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24, alignItems: "center" }}>
+    <View style={{ flex: 1, padding: 24, alignItems: "center", backgroundColor: "#BCBCBC" }}>
 
       <TouchableOpacity
         style={[styles.buttons, { backgroundColor: "aqua", marginBottom: 50, width: 300 }]}
-        onPress={getUsers}
+        onPress={getClientes}
       >
-        <Text style={{ fontSize: 22 }}>Listado de Usuarios</Text>
+        <Text style={{ fontSize: 22 }}>Listado de Clientes</Text>
       </TouchableOpacity>
 
       <View style={{ alignItems: "center", marginBottom: 20 }}>
-        <Text style={{ fontWeight: "bold" }}>SEARCH USER</Text>
+        <Text style={{ fontWeight: "bold" }}>SEARCH CLIENT</Text>
         <TextInput
           style={[styles.inputs, { marginBottom: 30 }]}
           placeholder="Search by Id"
@@ -66,20 +157,42 @@ export default function App() {
         />
         <TextInput
           style={styles.inputs}
-          value={name}
+          value={nombre}
+          onChangeText={nombre => setNombre(nombre)}
         />
         <TextInput
           style={styles.inputs}
-          value={username}
+          value={apellidos}
+          onChangeText={apellidos => setApellidos(apellidos)}
         />
       </View>
 
       <TouchableOpacity
-        style={[styles.buttons, { backgroundColor: "green", marginBottom: 50, width: 300 }]}
-        onPress={() => getUserById(idsearch)}
+        style={[styles.buttons, { backgroundColor: "#71BA31", marginBottom: 5, width: 200 }]}
+        onPress={() => saveCliente()}
       >
-        <Text style={{ fontSize: 22 }}>Search</Text>
+        <Text style={{ fontSize: 22, color: "white" }}>Guardar</Text>
       </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.buttons, { backgroundColor: "#71BA31", marginBottom: 5, width: 200 }]}
+        onPress={() => getClientById(idsearch)}
+      >
+        <Text style={{ fontSize: 22, color: "white" }}>Search</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.buttons, { backgroundColor: "#558CCF", marginBottom: 5, width: 200 }]}
+        onPress={() => updateCliente(idsearch)}
+      >
+        <Text style={{ fontSize: 22, color: "white" }}>Editar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.buttons, { backgroundColor: "#D14842", marginBottom: 50, width: 200 }]}
+        onPress={() => deleteCliente(idsearch)}
+      >
+        <Text style={{ fontSize: 22, color: "white" }}>Eliminar</Text>
+      </TouchableOpacity>
+      
+
 
       {isLoading ? <ActivityIndicator size="large" color="#000" /> : (
         <FlatList
@@ -89,9 +202,9 @@ export default function App() {
             <TouchableOpacity
               style={[styles.buttons, { backgroundColor: "green" }]}
               onPress={() => {
-                alert(item.email)
+                alert(item.nombre)
               }}>
-              <Text style={{ color: "white" }}>{item.name}, {item.username}</Text>
+              <Text style={{ color: "white" }}>{item.nombre}, {item.apellidos}</Text>
             </TouchableOpacity>
 
           )}
@@ -115,13 +228,16 @@ const styles = StyleSheet.create({
     margin: 3,
     height: 30,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
 
   inputs: {
     borderBottomColor: "black",
     borderColor: "white",
     textAlign: "center",
+    borderTopColor: "#BCBCBC",
+    borderLeftColor: "#BCBCBC",
+    borderRightColor: "#BCBCBC",
     borderWidth: 1,
     height: 30,
     width: 300
